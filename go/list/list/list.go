884382cae36c334dbc131f567bf5bfa2e5c716d3
Royese_hudeland <2065835296@qq.com>
2024-09-26 22:58:49 +0800

    hashover,stackover

--- go/list/list/list.go
+++ go/list/list/list.go
@@ -2,65 +2,119 @@ package list
 
 import "fmt"
 
-type node[T int | string | float64 | float32] struct {
-	Index int
-	Value T
-	Next  *node[T]
-	Prev  *node[T]
+type Chainfunc interface {
+	Tranversal()
+	Append(initValue any)
+	RemoveAt(index int)
+	Change(index int, value any)
+	Find(value any)
+	Check()
 }
-type Chain[T int | string | float64 | float32] struct {
-	Head  *node[T]
-	Tail  *node[T]
-	count int
+type Nodefunc interface {
+	Show()
 }
 
-func (c *Chain[T]) Tranversal() {
+type HashTableFunc interface {
+	CheckHash()
+}
+type HashTable struct {
+	capacity int
+	bucket   [][]Node
+}
+
+type Node struct {
+	Index int
+	Value any
+	Next  *Node
+	Prev  *Node
+}
+type Chain struct {
+	Head   *Node
+	Tail   *Node
+	Hash   HashTable
+	Length int
+}
+
+func (h HashTable) Hashfunc(Key int) int {
+	return Key % h.capacity
+}
+
+func (n Node) Show() {
+	fmt.Println(n.Index, n.Value)
+}
+
+func (c *Chain) Tranversal() {
 	p := c.Head
-	p = p.Next
-	for i := 0; i <= c.count; i++ {
+	for i := 0; i <= c.Length; i++ {
 		fmt.Println(p.Index, p.Value)
 		p = p.Next
 	}
 }
 
-// func (c *Chain) DeleteNode(index int) {
-// 	p := c.Head
-// 	for i := 0; i < index; i++ {
-// 		p = p.Prev
-// 	}
-// 	cp := p.Prev
-// 	for i := p.Index; i <= c.count-1; i++ {
-// 		cp.Index--
-// 		cp = cp.Prev
-// 	}
-// 	p.Prev.Next = p.Next
-// 	p.Next.Prev = p.Prev
-// 	c.count--
-// }
-// func (c *Chain) Setvalue(index int, value string) {
-// 	p := c.Head
-// 	for i := 0; i < index; i++ {
-// 		p = p.Next
-// 	}
-// 	p.Value = value
-// }
+func (c *Chain) Find(index int) *Node {
+	key := c.Hash.Hashfunc(index)
+	bucket := c.Hash.bucket[key]
+	for i, v := range bucket {
+		if v.Index == index {
+			fmt.Println(v.Index, v.Value)
+			x := &c.Hash.bucket[key][i]
+			return x
+		}
+	}
+	fmt.Println("Not Found")
+	return nil
+}
 
-func (c *Chain[T]) Append(initValue T) {
-	p := &node[T]{0, initValue, nil, nil}
+func (c *Chain) Check() {
+	p := c.Head
+	for i := 0; i <= c.Length; i++ {
+		fmt.Println(p)
+		p = p.Next
+	}
+	fmt.Println()
+	n := c.Tail
+	for i := 0; i < c.Length; i++ {
+		fmt.Println(n)
+		n = n.Prev
+	}
+}
+func (c *Chain) RemoveAt(index int) {
+	find := c.Find(index)
+	p := find.Prev
+	n := p.Next.Next
+	n.Prev = p
+	p.Next = n
+	c.Length--
+}
+
+func (c *Chain) Append(initValue any) {
+	p := &Node{0, initValue, nil, nil}
 	p.Value = initValue
 	c.Tail.Next = p
 	p.Prev = c.Tail
 	c.Tail = p
 	c.Tail.Next = c.Head
-	c.count++
-	p.Index = c.count
+	c.Length++
+	p.Index = c.Length
+	key := c.Hash.Hashfunc(p.Index)
+	c.Hash.bucket[key] = append(c.Hash.bucket[key], *p)
 }
 
-func Create[T int | string | float64 | float32](initValue T) Chain[T] {
-	head := &node[T]{0, initValue, nil, nil}
+func (c HashTable) CheckHash() {
+	for i, v := range c.bucket {
+		for j, n := range v {
+			fmt.Println(i, j, n)
+		}
+	}
+}
+
+func Create(initValue any) Chain {
+	head := &Node{0, initValue, nil, nil}
 	tail := head
 	tail.Next = head
 	tail.Prev = nil
-	Chain := Chain[T]{head, tail, 0}
+	hashlist := HashTable{10, make([][]Node, 10)}
+	hashlist.bucket[0] = append(hashlist.bucket[0], *head)
+	Chain := Chain{head, tail, hashlist, 0}
 	return Chain
 }
--- go/list/list/list_test.go
+++ go/list/list/list_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func Test_ChainTable(t *testing.T) {
-	chain1 := Create[string]("Chain1")
+	chain1 := Create("Chain1")
 	fmt.Printf("Head: %v\n", chain1)
 	chain1.Append("Node1")
 	chain1.Append("Node2")
 	chain1.Append("Node3")
 	chain1.Append("Node4")
+	chain1.RemoveAt(2)
+	fmt.Println()
 	chain1.Tranversal()
 }
