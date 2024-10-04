0509248e7c596bb3709b4a0950147138507ba034
Royese_hudeland <2065835296@qq.com>
2024-10-04 16:57:32 +0800

    ver 1.20

---
+++ go/list/list/interface.go
@@ -0,0 +1,23 @@
+package list
+
+type LinkedList[T comparable] interface {
+	Find(Val T) (*ListNode[T], bool)
+	// find a node with value Val in the list, return the node and a bool to indicate whether it was found or not
+
+	Add(Val T)
+	// add a new node with value Val to the head of the list
+
+	Delete(Val T, index int) error
+}
+
+type Iterat[T comparable] interface {
+	Begin() Iterator[T]
+
+	End() Iterator[T]
+
+	Next() Iterator[T]
+
+	Get() T
+
+	IsEmpty() bool
+}
--- go/list/list/list_test.go
+++ go/list/list/list_test.go
@@ -5,14 +5,32 @@ import (
 	"testing"
 )
 
-func Test_ChainTable(t *testing.T) {
-	chain1 := Create("Chain1")
-	fmt.Printf("Head: %v\n", chain1)
-	chain1.Append("Node1")
-	chain1.Append("Node2")
-	chain1.Append("Node3")
-	chain1.Append("Node4")
-	chain1.RemoveAt(2)
-	fmt.Println()
-	chain1.Tranversal()
+func TestList(t *testing.T) {
+	list1 := Create("apple")
+	list1.Add("banana")
+	list1.Add("orange")
+	list1.Add("apple")
+	x, ok := list1.Find("apple")
+	if ok {
+		fmt.Println("Found apple", x)
+	} else {
+		fmt.Println("Not found apple")
+	}
+	list1.Delete("banana", 0)
+	Iter := list1.Begin()
+	for !Iter.IsEmpty() {
+		fmt.Println(Iter.Pointer.Val)
+		Iter = Iter.Next()
+	}
+	list1.Add("banana")
+	err := list1.Delete("grape", 0)
+	fmt.Println(err)
+	err = list1.Delete("apple", 2)
+	fmt.Println(err)
+	list1.Delete("apple", 1)
+	Iter = list1.Begin()
+	for !Iter.IsEmpty() {
+		fmt.Println(Iter.Pointer.Val)
+		Iter = Iter.Next()
+	}
 }
